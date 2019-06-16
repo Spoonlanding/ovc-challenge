5fe8424698afa9f3cd48ae95e734caa38cc3e38a
Spencer Mitchell <smitch.work@gmail.com>
2019-06-16 14:22:29 -0500

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,15 @@ import store from "./store";
 import UserDataTable from "./components/UserDataTable";
 
 function App(): JSX.Element {
+  const githubUrl = "https://github.com/Spoonlanding/ovc-challenge";
+  const linkedInUrl = "https://www.linkedin.com/in/spoonlanding/";
   return (
     <div className="App">
+      <h1 id="pageHeader">OVC Coding Challenge</h1>
+      <h3 id="subHeader">
+        Submission by <a href={linkedInUrl}>Spencer Mitchell</a> |{" "}
+        <a href={githubUrl}>View on GitHub</a>
+      </h3>
       <UserDataTable />
     </div>
   );
